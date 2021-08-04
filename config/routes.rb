b7a9348910321a4cda80c442937a4f7827e18655
Dex <fitch.dexter@gmail.com>
2021-08-03 17:23:46 -0700

    Add preliminary controller and routes for portfolio entries

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#index'
 
-
+  namespace :api, defaults: { format: 'json' } do
+    resources :entries, only: :index
+  end
   
   match '*path', to: 'pages#index', via: :all
 end
